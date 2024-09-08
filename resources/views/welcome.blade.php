7b4583c8a095e4e56fb2d9fee041eeb6f9d57255
Triya624 <triyaarrestyan20@gmail.com>
2024-09-08 22:28:33 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('heading', 'Dashboard')
+
+@section('title', 'Dashboard')
+
+@section('content')    
+    <div class="card-header">
+
+    </div>
+    <div class="card-body">
+        <h1 class="text text-gray-500">Selamat Datang di Aplikasi Penjualan Barang</h1>
+    </div>
+@endsection
